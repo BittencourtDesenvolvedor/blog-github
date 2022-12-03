8c164c8f73afaa3395819a50d0d3fd02a6c78fd8
Roberto Bittencourt <bittencourt.desenvolvedor@gmail.com>
2022-12-03 18:31:57 -0300

    atualizado dia 03/10/2022

--- controlles/CategoriesController.js
+++ controlles/CategoriesController.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../db/conn');
+const slugify = require('slugify')
 
 router.get('/categories', (req, res) => {
-    res.render('admin/categories')
+    conn.select().table('categories').then(categ => {
+       res.render('admin/categories', {categ: categ}) 
+    })
+    
 })
 
 router.get('/categories/news', (req, res) => {
     res.render('admin/categories/new')
+})
+
+router.post('/categories/news', (req, res) => {
+       var title = req.body.title;
+       if(title != ""){
+          conn.insert({
+             title: title,
+             slug: slugify(title)
+            }).table('categories').then(() => {
+                res.redirect('/')
+            }).catch(error => {
+                res.redirect('/categories/news')
+            })
+       }else{
+         res.redirect('/categories/news')
+       }
 })
 
 
